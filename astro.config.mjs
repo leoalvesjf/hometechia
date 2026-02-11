9c05e5a115b2dbafc8260774625746b875fda456
eloaelv <leonardo.alves@ericsson.com>
2026-02-11 08:06:08 -0300

    feat: add astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://leoalvesjf.github.io',
-    base: '/hometechia',
+    site: 'https://www.hometechia.com.br',
+    integrations: [sitemap()]
 });
