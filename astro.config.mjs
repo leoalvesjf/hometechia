993f10038813225fae13310af1ff319df5bf75d0
eloaelv <leonardo.alves@ericsson.com>
2026-02-11 09:48:34 -0300

    bugfix: ajuste na url de rota global

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://www.hometechia.com.br',
+    base: '/', // Adicione esta linha
     integrations: [sitemap()]
 });
